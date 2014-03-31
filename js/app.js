b636cf5955d2868d38512fb97f2bc7035af4a2a1
Jef Mayer <modeseventyeight@gmail.com>
2014-03-31 10:21:00 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// Requires jquery, modernizr
+(function ($, window, document, undefined) {
+	$(function () {
+		var app = new App();
+		app.init();		
+	});
+})(jQuery, window, document);
+
+var App = function App() {
+			
+	this.init = function() {
+		// var gallery = $('.ra-video-gallery').videogallery();
+	};
+	
+	
+	
+	
+	return this;
+}
